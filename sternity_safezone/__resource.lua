15428e21ac565c871da767ede38be2bf0193a3a3
Sternity <93495952+Sternity@users.noreply.github.com>
2021-12-21 20:59:55 +0100

    Add files via upload

---
+++ sternity_safezone/__resource.lua
@@ -0,0 +1,8 @@
+--Script de zona segura BASADO en el de SpainRP reutilizando el script es_carwash.
+--Es un script muy sencillo, no hay mucho misterio tras él. Pero si lo compartes, da créditos por favor. Sternity#8894 | NarcoSpainRP - discord.gg/d6QX8rUATU
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Zonas segura BASADA en la de SpainRP hecho por Sternity#8894 reutilizando el script es_carwash'
+
+client_script 'es_carwash_client.lua'
+server_script 'es_carwash_server.lua'
---
+++ sternity_safezone/es_carwash_client.lua
@@ -0,0 +1,32 @@
+--Script de zona segura BASADO en el de SpainRP reutilizando el script es_carwash.
+--Es un script muy sencillo, no hay mucho misterio tras él. Pero si lo compartes, da créditos por favor. Sternity#8894 | NarcoSpainRP - discord.gg/d6QX8rUATU
+
+vehicleWashStation = {
+	{222.3371, -793.5749, 31.7272}
+}
+
+function es_carwash_DrawSubtitleTimed(m_text, showtime)
+	SetTextEntry_2('STRING')
+	AddTextComponentString(m_text)
+	DrawSubtitleTimed(showtime, 1)
+end
+
+function es_carwash_DrawNotification(m_text)
+	SetNotificationTextEntry('STRING')
+	AddTextComponentString(m_text)
+	DrawNotification(true, false)
+end
+
+Citizen.CreateThread(function ()
+	while true do
+		Citizen.Wait(0)
+		if Player(GetPlayerPed(-1)) then 
+			for i = 1, #vehicleWashStation do
+				garageCoords2 = vehicleWashStation[i]
+				if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), garageCoords2[1], garageCoords2[2], garageCoords2[3], true ) < 35 then
+					es_carwash_DrawSubtitleTimed("Estás en una zona de ~b~relax")
+				end
+			end
+		end
+	end
+end)
