8bc7ac52f71e3e23d45ca0dd81681ef4115cd066
Sternity <93495952+Sternity@users.noreply.github.com>
2021-12-21 20:59:03 +0100

    Delete sternity_safezone directory

--- sternity_safezone/__resource.lua
+++
@@ -1,6 +0,0 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-
-description 'Zonas segura BASADA en la de SpainRP hecho por Sternity#8894 reutilizando el script es_carwash'
-
-client_script 'es_carwash_client.lua'
-server_script 'es_carwash_server.lua'
